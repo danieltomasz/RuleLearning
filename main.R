a26bde04104f984c99949936d902ec5fe3ad7dac
Daniel Borek <dtborek@gmail.com>
2015-06-24 13:36:34 +0200

    working version

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 script.dir <- dirname(sys.frame(1)$ofile)
 setwd(script.dir)
 source("code/functions.R")
-source("code/mediate.r")
+source("code/mediateStep.r")
 source("code/preparations.R")
 source("code/compLik.r")
 
